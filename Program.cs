35cd9a6402f668ed45b6e88045e0a77bf7efe647
sebas0907 <39376403+sebas0907@users.noreply.github.com>
2020-06-25 23:13:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,16 +9,14 @@ namespace Dsolver_RK4//solve a coupled spring-mass system:
 {
         class Program
         {
-            // Declaration of one or many differential equations as single functions of the arguments (x,v):
+            // Declaration of one or many differential equations as single functions of the arguments (x1,x2):
             static double F1(double x1, double x2)
             {
             return -2 * x1 + x2;
-            //return -(1 - 2*M / R) / 2;
             }
             static double F2(double x1, double x2)
             {
             return x1 -2 * x2;
-            //return -(100*M*(R-2*M)*(2*R-3*M)) / (48*Math.PI*R * R*(R*R*(R-2*M)+(R-M)/12*Math.PI));
             }
             static void Main(string[] args)
             {// User input of parameters from intial conditions x0, y0, step size h and number of steps N:         
@@ -36,11 +34,11 @@ namespace Dsolver_RK4//solve a coupled spring-mass system:
                 Console.WriteLine("Enter number of steps:\nN= ");
                 int N = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Results using the Euler´s method:\nt\t x1(t)\t x2(t)\t v1(t)\t v2(t) ");
-                //To store the values of the Euler´s algorithm in a .txt file:
+                //To store the values of the solution in a .txt file:
                 // Set a variable "pfad" to the Document´s path.
                 string pfad = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
-                // Write result to a new file named "rk4.txt".
+                // Write result to a new file named "spring.txt".
                 using (StreamWriter outputFile = new StreamWriter(Path.Combine(pfad, "spring.txt")))
                 {
                     for (int i = 0; i < N; i++)
@@ -60,19 +58,13 @@ namespace Dsolver_RK4//solve a coupled spring-mass system:
                     double v1n = v1 + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
                     double v2n = v2 + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
                     double x1n = x1 + v1 * h;
-                    double x2n = x2 + v2 * h;
-                    //double Rn = R + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-                    //double Mn = M + (l1 + 2 * l2 + 2 * l3 + l4) / 6;
+                    double x2n = x2 + v2 * h;                 
                         v1 = v1n;
                         v2 = v2n;
                         x1 = x1n;
                         x2 = x2n;
-                        //R = Rn;
-                        //M = Mn;
                     // The file output is given columns with a limited number of floating points:
                     outputFile.WriteLine("{0:n3}\t{1:n3}\t{2:n3}\t{3:n3}\t{4:n4}", t, x1, x2, v1, v2);
-                    //outputFile.WriteLine("{0:n3}\t{1:n3}\t{2:n3}\t{3:n3}\t{4:n3}", u, R, M, k1/h, l1/h);
-                    //Console.WriteLine("{0:n3}\t{1:n3}\t{2:n3}\t{3:n3}\t{4:n3}", u, R, M, k1/h, l1/h);
                     //Display result in the console for verification:
                     Console.WriteLine("{0:n3}\t{1:n3}\t{2:n3}\t{3:n3}\t{4:n4}", t, x1, x2, v1, v2);
                     }
